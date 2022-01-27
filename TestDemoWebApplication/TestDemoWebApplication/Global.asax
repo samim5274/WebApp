dca62b38fc26c05fab60596b8069f4ec6f46a715
Samim Hossen <banglarcele1122@gmail.com>
2022-01-27 22:59:43 +0600

    1

---
+++ TestDemoWebApplication/TestDemoWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestDemoWebApplication.MvcApplication" Language="C#" %>
---
+++ TestDemoWebApplication/TestDemoWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestDemoWebApplication.Startup))]
+namespace TestDemoWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
